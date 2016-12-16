8cc35a808e78ce14ddbc7796918f274e83799daf
M-Mueller <M-Mueller@users.noreply.github.com>
2016-12-16 11:55:43 +0100

    Add optional maximum octree depth

--- plugin/include/Octree.h
+++ plugin/include/Octree.h
@@ -8,38 +8,60 @@
 
 namespace collage
 {
-    class Image;
-    class BoundingBox;
+	class Image;
+	class BoundingBox;
 
-    class Octree : public Entity
-    {
-        Q_OBJECT
-        Q_PROPERTY(collage::Image* volume READ volume WRITE setVolume NOTIFY volumeChanged)
+	/**
+	 * @brief The Octree class structures a volume into a hierachy of boxes.
+	 * Each level of the hierachy splits the volume into up to 8 smaller boxes,
+	 * and contains the minimum and maximum value of the volume inside its box.
+	 * When rendering, only boxes on the lowest level that overlap with the
+	 * target min/max range are rendered.
+	 * The box coordinates are stored in world coordinates.
+	 */
+	class Octree : public Entity
+	{
+		Q_OBJECT
+		Q_PROPERTY(collage::Image* volume READ volume WRITE setVolume NOTIFY volumeChanged)
+		/**
+		 * @brief Maximum number of octree levels.
+		 * A value of -1 means unlimited number of levels.
+		 * A value of 0 means only one top level.
+		 * Default is -1.
+		 */
+		Q_PROPERTY(int maxDepth READ maxDepth WRITE setMaxDepth NOTIFY maxDepthChanged)
 
-    public:
-        Octree(QObject* parent=0);
-        virtual ~Octree();
+	public:
+		Octree(QObject* parent=0);
+		virtual ~Octree();
 
-        void synchronize() override;
-        void render(GlProgram& program) override;
+		void synchronize() override;
+		void render(GlProgram& program) override;
 
-        Image* volume() const;
+		Image* volume() const;
 
-    public slots:
-        void setVolume(Image* volume);
-        void compute();
+		int maxDepth() const;
 
-    signals:
-        void volumeChanged(Image* volume);
-        void completed();
+	public slots:
+		void setVolume(Image* volume);
+		void setMaxDepth(int maxDepth);
 
-    private:
-        Image* _volume;
+		void compute();
 
-        struct Node;
-        typedef std::unique_ptr<Node> NodePtr;
-        NodePtr _root;
-    };
+	signals:
+		void volumeChanged(Image* volume);
+		void maxDepthChanged(int maxDepth);
+
+		void completed();
+
+	private:
+		Image* _volume;
+		int _maxDepth;
+
+		struct Node;
+		typedef std::unique_ptr<Node> NodePtr;
+		NodePtr _root;
+	};
 }
 
 #endif // OCTREE_H
--- plugin/src/Octree.cpp
+++ plugin/src/Octree.cpp
@@ -6,202 +6,228 @@
 
 namespace collage
 {
-    struct Octree::Node
-    {
-        double min, max;
-        BoundingBox bb;
-        int level;
-        std::vector<NodePtr> children;
-    };
+	struct Octree::Node
+	{
+		double min, max;
+		BoundingBox bb;
+		int level;
+		std::vector<NodePtr> children;
+	};
 
-    Octree::Octree(QObject* parent):
-        Entity(parent),
-        _volume(nullptr)
-    {
+	Octree::Octree(QObject* parent):
+		Entity(parent),
+		_volume(nullptr),
+		_maxDepth(-1)
+	{
 
-    }
+	}
 
-    Octree::~Octree()
-    {
+	Octree::~Octree()
+	{
 
-    }
+	}
 
-    void Octree::synchronize()
-    {
-        if(_root)
-        {
-            std::function<void(NodePtr&)> sync = [&](NodePtr& node)
-            {
-                node->bb.synchronize();
-                for(auto& c: node->children)
-                    sync(c);
-            };
+	void Octree::synchronize()
+	{
+		if(_root)
+		{
+			std::function<void(NodePtr&)> sync = [&](NodePtr& node)
+			{
+				node->bb.synchronize();
+				for(auto& c: node->children)
+					sync(c);
+			};
 
-            sync(_root);
-        }
-    }
+			sync(_root);
+		}
+	}
 
-    void Octree::render(GlProgram& program)
-    {
-        QList<QColor> colors = {
-            Qt::red,
-            Qt::green,
-            Qt::blue,
-            Qt::magenta,
-            Qt::yellow,
-            Qt::gray,
-            Qt::cyan,
-            Qt::darkBlue,
-            Qt::darkRed
-        };
+	void Octree::render(GlProgram& program)
+	{
+		QList<QColor> colors = {
+			Qt::red,
+			Qt::green,
+			Qt::blue,
+			Qt::magenta,
+			Qt::yellow,
+			Qt::gray,
+			Qt::cyan,
+			Qt::darkBlue,
+			Qt::darkRed
+		};
 
-        if(_root)
-        {
-            std::function<void(NodePtr&)> render = [&](NodePtr& node)
-            {
-                if(node->min <= 0.5 && node->max >= 0.5)
-                {
-                    if(node->children.empty())
-                    {
-                        node->bb.render(program);
-                    }
-                    else
-                    {
-                        for(auto& c: node->children)
-                            render(c);
-                    }
-                }
-            };
+		if(_root)
+		{
+			std::function<void(NodePtr&)> render = [&](NodePtr& node)
+			{
+				if(node->min <= 0.5 && node->max >= 0.5)
+				{
+					if(node->children.empty())
+					{
+						node->bb.render(program);
+					}
+					else
+					{
+						for(auto& c: node->children)
+							render(c);
+					}
+				}
+			};
 
-            render(_root);
-        }
-    }
+			render(_root);
+		}
+	}
 
-    Image* Octree::volume() const
-    {
-        return _volume;
-    }
+	Image* Octree::volume() const
+	{
+		return _volume;
+	}
 
-    void Octree::setVolume(Image* volume)
-    {
-        if (_volume == volume)
-            return;
+	int Octree::maxDepth() const
+	{
+		return _maxDepth;
+	}
 
-        _volume = volume;
-        emit volumeChanged(volume);
+	void Octree::setVolume(Image* volume)
+	{
+		if (_volume == volume)
+			return;
 
-        compute();
-    }
+		_volume = volume;
+		emit volumeChanged(volume);
 
-    void Octree::compute()
-    {
-        if(!_volume)
-            return;
+		compute();
+	}
 
-        auto root = std::make_unique<Node>();
-        root->bb.setRenderMode(BoundingBox::Solid);
-        root->level = 0;
-        root->bb.setCenter(QVector3D(0, 0, 0));
-        root->bb.setSize(_volume->size());
+	void Octree::setMaxDepth(int maxLevels)
+	{
+		if (_maxDepth == maxLevels)
+			return;
 
-        QVector3D minBBSize = QVector3D(8, 8, 8);
-        QVector3D centerOffset = _volume->size()/2;
+		_maxDepth = maxLevels;
+		emit maxDepthChanged(maxLevels);
+	}
 
-        std::function<void(NodePtr&)> split = [&](NodePtr& parent)
-        {
-            QVector3D size = parent->bb.size();
-            QVector3D childSize = size/2.0;
-            QVector3D center = parent->bb.center();
+	void Octree::compute()
+	{
+		if(!_volume)
+			return;
 
-            // minimum size reached
-            if(childSize.x() < minBBSize.x() && childSize.y() < minBBSize.y() && childSize.z() < minBBSize.z())
-                return;
+		auto root = std::make_unique<Node>();
+		root->bb.setRenderMode(BoundingBox::Solid);
+		root->level = 0;
+		root->bb.setCenter(QVector3D(0, 0, 0));
+		root->bb.setSize(_volume->size());
 
-            std::vector<int> sx, sy, sz;
-            if(childSize.x() < minBBSize.x())
-                sx = {0};
-            else
-                sx = {-1, 1};
+		QVector3D minBBSize = QVector3D(8, 8, 8);
+		QVector3D centerOffset = _volume->size()/2;
 
-            if(childSize.y() < minBBSize.y())
-                sy = {0};
-            else
-                sy = {-1, 1};
+		std::function<void(NodePtr&)> split = [&](NodePtr& parent)
+		{
+			QVector3D size = parent->bb.size();
+			QVector3D childSize = size/2.0;
+			QVector3D center = parent->bb.center();
 
-            if(childSize.z() < minBBSize.z())
-                sz = {0};
-            else
-                sz = {-1, 1};
+			// minimum size reached
+			if(childSize.x() < minBBSize.x() && childSize.y() < minBBSize.y() && childSize.z() < minBBSize.z())
+				return;
 
-            for(int x: sx)
-            {
-                for(int y: sy)
-                {
-                    for(int z: sz)
-                    {
-                        auto child = std::make_unique<Node>();
-                        child->bb.setRenderMode(BoundingBox::Solid);
-                        child->bb.setSize(childSize);
-                        child->bb.setCenter(center + QVector3D(x, y, z)*0.5*childSize);
-                        child->level = parent->level + 1;
-                        parent->children.push_back(std::move(child));
-                    }
-                }
-            }
+			if (_maxDepth != -1 && parent->level >= _maxDepth)
+				return;
 
-            for(auto& c: parent->children)
-                split(c);
-        };
-        split(root);
+			// determine in which directions we can still split
+			std::vector<int> sx, sy, sz;
+			if(childSize.x() < minBBSize.x())
+				sx = {0}; // don't split
+			else
+				sx = {-1, 1}; // split in left/right
 
-        std::function<void(NodePtr&)> computeMinMax = [&](NodePtr& node)
-        {
-            if(!node->children.empty())
-            {
-                for(auto& c: node->children)
-                    computeMinMax(c);
-                node->min = node->children[0]->min;
-                node->max = node->children[0]->max;
-                for(auto& c: node->children)
-                {
-                    node->min = std::min(node->min, c->min);
-                    node->max = std::max(node->max, c->max);
-                }
-            }
-            else
-            {
-                QVector3D offset = node->bb.center() - 0.5*node->bb.size();
-                QVector3D size = node->bb.size();
+			if(childSize.y() < minBBSize.y())
+				sy = {0};
+			else
+				sy = {-1, 1};
 
-                int zStart = static_cast<int>(std::floor(offset.z() + centerOffset.z()));
-                int zEnd = static_cast<int>(std::ceil(zStart + size.z()));
-                int yStart = static_cast<int>(std::floor(offset.y() + centerOffset.y()));
-                int yEnd = static_cast<int>(std::ceil(yStart + size.y()));
-                int xStart = static_cast<int>(std::floor(offset.x() + centerOffset.x()));
-                int xEnd = static_cast<int>(std::ceil(xStart + size.x()));
+			if(childSize.z() < minBBSize.z())
+				sz = {0};
+			else
+				sz = {-1, 1};
 
-                node->min = _volume->normalizedValue(xStart, yStart, zStart, 0);
-                node->max = node->min;
+			// split current boundingbox into sub-boundingboxes
+			for(int x: sx)
+			{
+				for(int y: sy)
+				{
+					for(int z: sz)
+					{
+						auto child = std::make_unique<Node>();
+						child->bb.setRenderMode(parent->bb.renderMode());
+						child->bb.setSize(childSize);
+						child->bb.setCenter(center + QVector3D(x, y, z)*0.5*childSize);
+						child->level = parent->level + 1;
+						parent->children.push_back(std::move(child));
+					}
+				}
+			}
 
-                for(int z=zStart; z<zEnd; ++z)
-                {
-                    for(int y=yStart; y<yEnd; ++y)
-                    {
-                        for(int x=xStart; x<xEnd; ++x)
-                        {
-                            double value = _volume->normalizedValue(x, y, z, 0);
-                            node->min = std::min(node->min, value);
-                            node->max = std::max(node->max, value);
-                        }
-                    }
-                }
-            }
+			// recursively split the new sub-boundingboxes
+			for(auto& c: parent->children)
+				split(c);
+		};
+		split(root);
 
-            node->bb.setSize(node->bb.size() * _volume->spacing());
-            node->bb.setCenter(node->bb.center() * _volume->spacing());
-        };
-        computeMinMax(root);
+		std::function<void(NodePtr&)> computeMinMax = [&](NodePtr& node)
+		{
+			if(!node->children.empty())
+			{
+				// if the node has children, its min/max values are the overall
+				// min/max values of its children
+				for(auto& c: node->children)
+					computeMinMax(c);
+				node->min = node->children[0]->min;
+				node->max = node->children[0]->max;
+				for(auto& c: node->children)
+				{
+					node->min = std::min(node->min, c->min);
+					node->max = std::max(node->max, c->max);
+				}
+			}
+			else
+			{
+				// compute the min/max values inside the bounding box
+				QVector3D offset = node->bb.center() - 0.5*node->bb.size();
+				QVector3D size = node->bb.size();
 
-        std::swap(root, _root);
-    }
+				int zStart = static_cast<int>(std::floor(offset.z() + centerOffset.z()));
+				int zEnd = static_cast<int>(std::ceil(zStart + size.z()));
+				int yStart = static_cast<int>(std::floor(offset.y() + centerOffset.y()));
+				int yEnd = static_cast<int>(std::ceil(yStart + size.y()));
+				int xStart = static_cast<int>(std::floor(offset.x() + centerOffset.x()));
+				int xEnd = static_cast<int>(std::ceil(xStart + size.x()));
+
+				node->min = _volume->normalizedValue(xStart, yStart, zStart, 0);
+				node->max = node->min;
+
+				for(int z=zStart; z<zEnd; ++z)
+				{
+					for(int y=yStart; y<yEnd; ++y)
+					{
+						for(int x=xStart; x<xEnd; ++x)
+						{
+							double value = _volume->normalizedValue(x, y, z, 0);
+							node->min = std::min(node->min, value);
+							node->max = std::max(node->max, value);
+						}
+					}
+				}
+			}
+
+			// convert from pixel to world coordinates after accessing pixel values
+			// (otherwise we need to contantly convert coordinates)
+			node->bb.setSize(node->bb.size() * _volume->spacing());
+			node->bb.setCenter(node->bb.center() * _volume->spacing());
+		};
+		computeMinMax(root);
+
+		std::swap(root, _root);
+	}
 }
